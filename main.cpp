8222c7b9ee0f57a335e2ad5b95eb400db5d457b4
S5619397 <118506564+S5619397@users.noreply.github.com>
2022-12-02 09:53:57 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
-#include <iostream>
+#include<iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
-  return 0;
+    int h=0, f=2;
+    cin>>h;
+bool a=numeroprimo(h, f);
+if ( a == true){
+    cout<<"numero primo";
+}
+else {
+    cout<<"numero non primo";
+    
+}
 }
