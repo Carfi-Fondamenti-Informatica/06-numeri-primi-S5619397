481a75e179e42bf5c40056ba1606e034efd0706b
S5619397 <118506564+S5619397@users.noreply.github.com>
2022-12-02 09:54:49 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,11 @@
 
+#include "lib.h"
+bool numeroprimo (int n, int i) {
+    
+    
+    if (n%i==0){
+    return i ==n;}
+    else 
+    {numeroprimo(n, i+1);}
+//return (n%i==0) ? i==n : numeroprimo(n,i+1);//
+}
